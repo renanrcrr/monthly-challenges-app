e8756520821695c9205ff2c77849c533441b542d
Msc R <renan@example.com>
2025-04-18 08:35:02 +0930

    Refactor monthly challenge retrieval to use separate dictionaries for string and integer month mappings

--- challenges/views.py
+++ challenges/views.py
@@ -3,29 +3,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def monthly_challenge_by_number(request, month):
-    month_challenges = {
-        1: "January Challenge",
-        2: "February Challenge",
-        3: "March Challenge",
-        4: "April Challenge",
-        5: "May Challenge",
-        6: "June Challenge",
-        7: "July Challenge",
-        8: "August Challenge",
-        9: "September Challenge",
-        10: "October Challenge",
-        11: "November Challenge",
-        12: "December Challenge",
-    }
-    if month > 12 or month < 1:
-        return HttpResponseNotFound("Invalid month")
-    else:
-        challenge_text = month_challenges[month]
-        return HttpResponse(challenge_text)
-
-def monthly_challenge(request, month):
-    month_challenges = {
+month_challenges_str = {
         "january": "January Challenge",
         "february": "February Challenge",
         "march": "March Challenge",
@@ -39,4 +17,28 @@ def monthly_challenge(request, month):
         "november": "November Challenge",
         "december": "December Challenge",
     }
-    return HttpResponse(month_challenges.get(month, "Invalid month"))
+
+month_challenges_int = {
+        1: "January Challenge",
+        2: "February Challenge",
+        3: "March Challenge",
+        4: "April Challenge",
+        5: "May Challenge",
+        6: "June Challenge",
+        7: "July Challenge",
+        8: "August Challenge",
+        9: "September Challenge",
+        10: "October Challenge",
+        11: "November Challenge",
+        12: "December Challenge",
+    }
+
+def monthly_challenge_by_number(request, month):
+    if month > 12 or month < 1:
+        return HttpResponseNotFound("Invalid month")
+    else:
+        challenge_text = month_challenges_int[month]
+        return HttpResponse(challenge_text)
+
+def monthly_challenge(request, month):
+    return HttpResponse(month_challenges_str.get(month, "Invalid month"))
