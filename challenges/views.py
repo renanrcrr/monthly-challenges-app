3fb0ab97a2a8bd54b49b6ec94185fa13e9e45e77
Msc R <renan@example.com>
2025-04-18 08:00:00 +0930

    Update monthly challenges URL and implement monthly challenge view

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!!!")
+def monthly_challenge(request, month):
+    month_challenges = {
+        "january": "January Challenge",
+        "february": "February Challenge",
+        "march": "March Challenge",
+        "april": "April Challenge",
+        "may": "May Challenge",
+        "june": "June Challenge",
+        "july": "July Challenge",
+        "august": "August Challenge",
+        "september": "September Challenge",
+        "october": "October Challenge",
+        "november": "November Challenge",
+        "december": "December Challenge",
+    }
+    return HttpResponse(month_challenges.get(month, "Invalid month"))
