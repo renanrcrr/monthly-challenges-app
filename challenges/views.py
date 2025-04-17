196c344efca60f1f7dade1e21f15bca15a077523
Msc R <renan@example.com>
2025-04-18 08:19:48 +0930

    Implement monthly challenge retrieval and validation in view

--- challenges/views.py
+++ challenges/views.py
@@ -4,7 +4,25 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    month_challenges = {
+        1: "January Challenge",
+        2: "February Challenge",
+        3: "March Challenge",
+        4: "April Challenge",
+        5: "May Challenge",
+        6: "June Challenge",
+        7: "July Challenge",
+        8: "August Challenge",
+        9: "September Challenge",
+        10: "October Challenge",
+        11: "November Challenge",
+        12: "December Challenge",
+    }
+    if month > 12 or month < 1:
+        return HttpResponseNotFound("Invalid month")
+    else:
+        challenge_text = month_challenges[month]
+        return HttpResponse(challenge_text)
 
 def monthly_challenge(request, month):
     month_challenges = {
